37e1ccade6569da528d40f433feb17d3356175a2
Hung Le <lethaihung142@gmail.com>
2024-12-06 15:03:57 +0700

    add variable for uploading management

--- config/view_data.py
+++ config/view_data.py
@@ -2,4 +2,6 @@ class view_data():
     def __init__(self):
         self.torrent_list = set([])
         self.choosen_torrent = None # For UI in view 1
+        self.choosen_torrent4 = None # For UI in View 4
         self.started_torrents = set([])
+        self.up_torrents = set([]) # Torrents that your side starts uploading file.
